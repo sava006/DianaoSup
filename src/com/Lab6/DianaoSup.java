3dbac8c6f50b6548ab5484d8ec117c5710affbd4
sava0066 <harrysavagespeaks@gmail.com>
2016-02-18 18:04:11 -0600

    this is my first commit

---
+++ src/com/Lab6/DianaoSup.java
@@ -0,0 +1,13 @@
+package com.Lab6;
+
+public class DianaoSup {
+
+    public static void main(String[] args) {
+	// write your code here
+        DinSau t_rex = new DinSau();
+        DinSau velociraptor = new DinSau();
+        t_rex.roar();
+        velociraptor.roar();
+
+    }
+}
---
+++ src/com/Lab6/DinSau.java
@@ -0,0 +1,9 @@
+package com.Lab6;
+
+public class DinSau {
+
+    public void roar() {
+	// write your code here
+        System.out.println("GRRR!!!");
+    }
+}
